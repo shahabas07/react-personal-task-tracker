b842ed06c8a5ac2740344829a413a2610f32646d
ShahabasAman <shezilaman@gmail.com>
2025-07-03 17:54:37 +0530

    chore: fix tailwind for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -8,3 +8,9 @@ export default defineConfig({
     tailwindcss(),
   ],
 })
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig({
+//   plugins: [react()],
+// })
